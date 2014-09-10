8a94678979516c6336ebae1d71cead2ea37b1876
user <40904@longroad.ac.uk>
2014-09-10 17:51:46 +0100

    Exercise 1.2 - Hello World

---
+++ Hello _World.py
@@ -0,0 +1,15 @@
+#Kavilan Naidoo
+#10-09-2014
+#Exercise 1.1 - Hello world
+
+
+
+print("Hello world")
+#This just prints the words in the speech marks.
+first_name= input("Please enter your name:")
+#This labels what ever you have put as your first name as "first_name".
+print(first_name)
+#This retrieves the information under the term "first_name".
+print("Hi {0}!" .format(first_name))
+#This rettrieves the information again from the term "first_name".
+
