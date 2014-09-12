d43a51d9cc0e0f8a1c1c106832a2998bdde5c410
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-09-12 14:34:06 +0100

    fahrenheit Exercise

---
+++ fahrenheit to degrees exercise.py
@@ -0,0 +1,8 @@
+#Kavilan Naidoo
+#12-09-2014
+#Fahrenheit to degerees Exercise
+
+fahrenheit= float(input(" Please enter the temperature in fahrenheit: "))
+centigrade= (fahrenheit-32)*(5/9)
+print("The degrees in centigrade is {0}".format(centigrade))
+
