c61f429c5abd3b3bbc54fc4982541e1997376d34
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-09-12 14:15:00 +0100

    Div and Mod Exercise

---
+++ Div and Mod Exercise.py
@@ -0,0 +1,11 @@
+#Kavilan Naidoo
+#12-09-2014
+#X_DIV_Y_X_MOD_Y
+
+number_1= int(input("Please enter a number: "))
+number_2= int(input("Please enter another number: "))
+div= number_1//number_2
+mod= number_1%number_2
+print("The whole number is {0} and the remainder is {1}.".format(div,mod))
+
+
