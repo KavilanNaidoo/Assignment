6e62c69a0e172dab6413b1bbabf308d2d9d59a80
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-09-12 13:43:47 +0100

    Exercises - 12/09/2014

---
+++ Total score Exercise.py
@@ -0,0 +1,7 @@
+#Kavilan Naidoo
+#12-09-2014
+#Total Score Exercise
+
+first_score= int(input("Please enter your first score"))
+second_score= int(input("Please enter your second score"))
+print("Your total score is {0} !").format(first_score + second_score)
