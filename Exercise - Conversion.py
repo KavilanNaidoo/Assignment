be6b13967cb268732e5b8f449d18107049cf42b1
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-09-16 12:05:45 +0100

    Exercise - Conversion

---
+++ Exercise - Conversion.py
@@ -0,0 +1,10 @@
+#Kavilan  Naidoo
+#16-09-2014
+#Exercise - Conversion
+
+
+
+inches=float(input("Please enter your height in inches: "))
+centremetres= inches * 2.54
+print("Your height in centremetres is: {0}".format(centremetres))
+
