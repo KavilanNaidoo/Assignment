b3bfb7c039bf425dbb817482d8b800b1b49e95ad
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-09-16 12:09:16 +0100

    Exercise - Conversion(1)

--- Exercise - Conversion.py
+++ Exercise - Conversion.py
@@ -8,3 +8,6 @@ inches=float(input("Please enter your height in inches: "))
 centremetres= inches * 2.54
 print("Your height in centremetres is: {0}".format(centremetres))
 
+stone=float(input("Please enter your weight in stone: "))
+kilograms= stone * 6.364
+print("Your weight in kilograms is: {0}".format(kilograms))
