845e459d35bd65e73c554beda0a22e4b4200a51b
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-09-19 14:31:19 +0100

    swimming pool maths exercise

---
+++ swimming pool maths exercise.py
@@ -0,0 +1,9 @@
+#Kavilan Naidoo
+#19-09-2014
+#swimming pool maths exercise
+
+import math
+depth=float(input("Please enter the depth of the pool: "))
+diameter=float(input("Please enter the diametre of the pool: "))
+volume = diameter*0.5*math.pi
+
