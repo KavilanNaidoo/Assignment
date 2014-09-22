d48d4e721f22381c0b0bfbcbf2deeb4a024c2d3b
KavilanNaidoo <40904@longroad.ac.uk>
2014-09-22 21:47:59 +0100

    driving exercise

---
+++ Driving exercise.py
@@ -0,0 +1,11 @@
+#Kavilan Naidoo
+#22/09/2014
+#Driving exercise
+
+age= int(input("Please enter your age: "))
+if age >17:
+    print("You are allowed to drive")
+if age <17:
+    print("You are not allowed to drive")
+if age ==17:
+    print("You are allowed to drive")
