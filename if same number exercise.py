2a8a1cf801530cf0d026b5c4d65344e2ef6a4782
KavilanNaidoo <40904@longroad.ac.uk>
2014-09-22 21:48:28 +0100

    if same number exercise

---
+++ if same number exercise.py
@@ -0,0 +1,13 @@
+#Kavilan Naidoo
+#22-09-2014
+#if same number exercise
+
+
+
+number = int(input("Please enter a number: "))
+number_2 = int(input(" Please enter another number: "))
+
+if number == number_2:
+    print("You have the right answer")
+
+    
